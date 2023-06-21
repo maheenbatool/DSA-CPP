ca8c175df12d2fbbbb31b641428c79a6160e154b
Maheen Batool <39929967+maheenbatool@users.noreply.github.com>
2023-06-21 21:33:32 +0100

    Create RemoveElement.cpp

---
+++ RemoveElement.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val)
+     {
+        int n = nums.size();
+        int nsize = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==val)
+                continue;
+            nums[nsize++] = nums[i];
+        }
+        return nsize;
+    }
+};
